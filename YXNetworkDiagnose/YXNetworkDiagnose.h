77ff235611671eb0e1ad3bd80df19c519a0cbcaf
YourtionGuo <yourtion@gmail.com>
2016-06-08 14:33:27 +0800

    add mac support

--- YXNetworkDiagnose/YXNetworkDiagnose.h
+++ YXNetworkDiagnose/YXNetworkDiagnose.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Yourtion. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for YXNetworkDiagnose.
 FOUNDATION_EXPORT double YXNetworkDiagnoseVersionNumber;
