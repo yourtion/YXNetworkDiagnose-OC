2c7ca7f1a1196f806b732f9373892730e269a43c
YourtionGuo <yourtion@gmail.com>
2016-06-23 13:18:49 +0800

    add test

--- YXNetworkDiagnoseTests/YXNDPingTest.m
+++ YXNetworkDiagnoseTests/YXNDPingTest.m
@@ -7,16 +7,23 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "YXNetworkDiagnose.h"
 
-@interface YXNDPingTest : XCTestCase
+@interface YXNDPingTest : XCTestCase <YXNDOutputDelegate>
 
 @end
 
+static int kTimeOut = 30;
+
 @implementation YXNDPingTest
+{
+    int _count;
+}
 
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    _count = 0;
 }
 
 - (void)tearDown {
@@ -24,16 +31,51 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testSimplePingStop {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Handler called"];
+    __block BOOL run = NO;
+    YXNDPing *ping = [YXNDPing start:@"www.baidu.com" output:self complete:^(YXNDPingResult *result) {
+        XCTAssertNotNil(result, @"Result can't be nil");
+        XCTAssertEqual(kYXNDRequestStoped, result.code, @"stop code");
+        run = YES;
+        [expectation fulfill];
+    }];
+    [ping stop];
+    [self waitForExpectationsWithTimeout:kTimeOut handler:nil];
+    XCTAssert(run, @"PASS");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testSimplePingTimeOut {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Handler called"];
+    __block BOOL run = NO;
+    [YXNDPing start:@"1.1.1.1" output:self complete:^(YXNDPingResult *result) {
+        XCTAssertNotNil(result, @"Result can't be nil");
+        run = YES;
+        [expectation fulfill];
+    } interval:200 count:1];
+    [self waitForExpectationsWithTimeout:kTimeOut*2 handler:nil];
+    XCTAssert(run, @"PASS");
+}
+
+- (void)testPing {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"Handler called"];
+    __block BOOL run = NO;
+    [YXNDPing start:@"www.baidu.com" output:self complete:^(YXNDPingResult *result) {
+        XCTAssertNotNil(result, @"Result can't be nil");
+        XCTAssertEqual(0, result.code, @"normal code");
+        XCTAssert(result.maxRtt >= result.avgRtt, @"max time >= avg time");
+        XCTAssert(result.minRtt <= result.avgRtt, @"min time =< avg time");
+        XCTAssertEqual(_count, 5);
+        run = YES;
+        [expectation fulfill];
+    } interval:200 count:3];
+    [self waitForExpectationsWithTimeout:kTimeOut handler:nil];
+    XCTAssert(run, @"PASS");
+}
+
+- (void)write:(NSString*)line{
+    NSLog(@"%@", line);
+    _count ++;
 }
 
 @end
