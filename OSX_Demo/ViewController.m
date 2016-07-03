a1b279155fecfb2889a6e4780fc332656e9ceef0
YourtionGuo <yourtion@gmail.com>
2016-07-03 10:16:56 +0800

    update Mac Demo

--- OSX_Demo/ViewController.m
+++ OSX_Demo/ViewController.m
@@ -10,46 +10,79 @@
 #import <YXNetworkDiagnose/YXNetworkDiagnose.h>
 
 @interface ViewController ()<YXNDOutputDelegate>
-
+@property (unsafe_unretained) IBOutlet NSTextView *result;
+@property (unsafe_unretained) IBOutlet NSTextField *pingText;
+@property (unsafe_unretained) IBOutlet NSTextField *tcpPingText;
+@property (unsafe_unretained) IBOutlet NSTextField *nslookupText;
+@property (unsafe_unretained) IBOutlet NSTextField *traceText;
+@property (unsafe_unretained) IBOutlet NSTextField *requestText;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [_result setString:[NSString stringWithFormat:@"%@DeviceIP: %@\n", _result.string , [YXNDNetworkInfo deviceIP]]];
+    [_result setString:[NSString stringWithFormat:@"%@LocalDNSServers: %@\n", _result.string , [YXNDNetworkInfo localDNSServers]]];
+    [_result setString:[NSString stringWithFormat:@"%@NetworkType: %lu\n", _result.string , (unsigned long)[YXNDNetworkInfo networkType]]];
+    [_result setString:[NSString stringWithFormat:@"%@NetworkDescription: %@", _result.string , [YXNDNetworkInfo networkDescription]]];
+}
+- (IBAction)ping:(id)sender {
+    [_result setString:@""];
+    NSString *src = _pingText.stringValue;
+    [YXNDPing start:src output:self complete:^(YXNDPingResult *result) {
+        NSLog(@"%@",result.description);
+    }];
+}
 
-    // Do any additional setup after loading the view.
-    [YXNDPing start:@"192.168.1.1" output:self complete:^(YXNDPingResult *result) {
+- (IBAction)tcpPing:(id)sender {
+    [_result setString:@""];
+    NSString *src = _tcpPingText.stringValue;
+    [YXNDPing start:src output:self complete:^(YXNDPingResult *result) {
         NSLog(@"%@",result.description);
     }];
-    [YXNDTcpPing start:@"qq.com" output:self complete:^(YXNDTcpPingResult *result) {
-        NSLog(@"%@",result.description);
-    }];
-    [YXNDTcpPing start:@"www.baidu.com" port:443 count:6 output:self complete:^(YXNDTcpPingResult *result) {
-        NSLog(@"%@",result.description);
-    }];
-    [YXNDNslookup start:@"qq.com" output:self complete:^(NSArray *result) {
+}
+
+- (IBAction)nslookup:(id)sender {
+    [_result setString:@""];
+    NSString *src = _nslookupText.stringValue;
+    [YXNDNslookup start:src output:self complete:^(NSArray *result) {
         for(YXNDRecord *res in result) {
             NSLog(@"%@",res.description);
         }
     }];
-    [YXNDTraceRoute start:@"qq.com" output:self complete:^(YXNDTraceRouteResult *result) {
-        NSLog(@"%@",result.description);
-    }];
-    [YXNDHttp start:@"http://baidu.com" output:self complete:^(YXNDHttpResult *result) {
-        NSLog(@"%@",result.description);
-    }];
-    NSLog(@"%@", [YXNDNetworkInfo externalIpInfo]);
 }
 
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
+- (IBAction)traceRoute:(id)sender {
+    [_result setString:@""];
+    NSString *src = _traceText.stringValue;
+    [YXNDTraceRoute start:src output:self complete:^(YXNDTraceRouteResult *result) {
+        NSLog(@"%@",result.description);
+    }];
+}
 
-    // Update the view, if already loaded.
+- (IBAction)httpRequest:(id)sender {
+    [_result setString:@""];
+    NSString *src = _requestText.stringValue;
+    [YXNDHttp start:src output:self complete:^(YXNDHttpResult *result) {
+        NSLog(@"%@",result.description);
+    }];
+}
+
+- (IBAction)ipInfo:(id)sender {
+    [_result setString:@""];
+    NSDictionary *res = [YXNDNetworkInfo externalIpInfo];
+    NSArray *keys = [res allKeys];
+    for (NSString *key in keys) {
+        [_result setString:[NSString stringWithFormat:@"%@%@ : %@\n",_result.string, key, [res objectForKey:key]]];
+    }
 }
 
 - (void)write:(NSString*)line {
     NSLog(@"%@", line);
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [_result setString:[NSString stringWithFormat:@"%@%@", _result.string , line]];
+    });
 }
 
 @end
